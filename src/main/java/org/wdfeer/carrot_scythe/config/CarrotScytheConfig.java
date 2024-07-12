c7b7a5bf03570222830335ae461d7b86de2bd5f6
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-12 13:44:23 +0200

    Fix numbers not being configurable

--- src/main/java/org/wdfeer/carrot_scythe/config/CarrotScytheConfig.java
+++ src/main/java/org/wdfeer/carrot_scythe/config/CarrotScytheConfig.java
@@ -3,8 +3,8 @@ package org.wdfeer.carrot_scythe.config;
 import eu.midnightdust.lib.config.MidnightConfig;
 
 public class CarrotScytheConfig extends MidnightConfig {
-    @Entry(category = "damage", min = 0.0)
-    public static Double logMultiplier = 1.0;
-    @Entry(category = "damage", min = 1.0)
-    public static Double linearDivisor = 1e5;
+    @Entry(min = 0.0)
+    public static double logMultiplier = 1.0;
+    @Entry(min = 1.0)
+    public static double linearDivisor = 1e5;
 }
